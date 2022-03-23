dbf457a0fb8f1899283604f68f17b37a14c354b1
Diego Moraes <dmoraes11cb@gmail.com>
2022-03-23 02:38:46 -0300

    release: 1.0.0 (#14)

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="prometeo-cli",
-    version="0.0.1",
+    version="1.0.0",
     author="Diego Moraes",
     author_email="dmoraes11cb@gmail.com",
-    description="CLI Tool for the Prometeo Open Banking AP",
+    description="CLI Tool for the Prometeo Open Banking API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/dieg0moraes/prometeo-cli",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
